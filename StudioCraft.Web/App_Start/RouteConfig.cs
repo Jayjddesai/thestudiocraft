f3a8f9771aee8c999c61ba1efebd1721fd08ba26
Jay Desai <jayjddesai@gmail.com>
2019-06-19 23:10:26 +0530

    1

---
+++ StudioCraft.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace StudioCraft.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add("Contact", new SeoFriendlyRoute("contact-us",
+            new RouteValueDictionary(new { controller = "Contact", action = "Index" }),
+            new MvcRouteHandler()));
+
+            routes.Add("About", new SeoFriendlyRoute("about-us",
+            new RouteValueDictionary(new { controller = "Contact", action = "Index" }),
+            new MvcRouteHandler()));
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
